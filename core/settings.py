bdca9219decf12e18e84a2ea2f97814bf12a5b2f
abimanyupewe <abimanyupw369@gmail.com>
2025-12-29 12:26:51 +0700

    fix: bind gunicorn to  and remove duplicate middleware

--- core/settings.py
+++ core/settings.py
@@ -69,7 +69,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
