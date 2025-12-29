8e2d4cf66774682dbb2ffe123484abb8090efe12
abimanyupewe <abimanyupw369@gmail.com>
2025-12-29 12:14:08 +0700

    feat: Add Django REST Framework authentication URLs.

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('api/tailor/', include('tailor.urls')),
     path('api/orders/', include('orders.urls')),
     path('api/reviews/', include('reviews.urls')),
+    path('api/auth/', include('rest_framework.urls')),
 ]
 
 if settings.DEBUG:
