b046da8d3b2ac34eaad33500226beaf72e5475f8
abimanyupewe <abimanyupw369@gmail.com>
2026-01-20 20:06:36 +0700

    feat: Add UserSerializer and integrate it into ReviewSerializer to display customer details for reviews.

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,12 +1,22 @@
 from rest_framework import serializers
 from .models import Review
 from orders.models import Order
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'avatar']
 
 class ReviewSerializer(serializers.ModelSerializer):
+    user = UserSerializer(source='order.customer', read_only=True)
+
     class Meta:
         model = Review
-        fields = ['id', 'order', 'rating', 'comment', 'created_at']
-        read_only_fields = ['id', 'created_at']
+        fields = ['id', 'order', 'user', 'rating', 'comment', 'created_at']
+        read_only_fields = ['id', 'created_at', 'user']
 
     def validate_order(self, value):
         if value.customer != self.context['request'].user:
